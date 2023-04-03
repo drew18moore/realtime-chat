6417f1aeccc5dac13f8d8921d42010fe29471973
Drew <dcm752052@gmail.com>
2023-04-03 01:01:22 -0400

    Changed tsconfig. Integrated prisma into app. Created db.ts to init client. Created /create endpoint to test db integration.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,18 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { db } from "./db";
+const PORT = 3000;
+const app = express();
 
-const PORT = 3000
-const app = express()
+app.use(cors())
+app.use(express.json())
+app.post("/create", async (req: Request, res: Response) => {
+  console.log(req.body);
+  const { username } = req.body;
+  const user = await db.user.create({ data: { username: username } });
+  res.status(200).json({ user })
+});
 
 app.listen(3000, () => {
   console.log(`Server running on port ${PORT}`);
-})
+});

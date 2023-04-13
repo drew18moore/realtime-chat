b608aac3ffe3b9c8db21cfe84e98aceb11d448be
Drew <dcm752052@gmail.com>
2023-04-12 23:21:24 -0400

    lastMessage and dateLastMessage now display properly.

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -3,10 +3,16 @@ import Converasation from "./Converasation";
 type SidebarProps = {
   conversations: Conversation[];
   currentConversation: Conversation | undefined;
-  setCurrentConversation: React.Dispatch<React.SetStateAction<Conversation | undefined>>;
+  setCurrentConversation: React.Dispatch<
+    React.SetStateAction<Conversation | undefined>
+  >;
 };
 
-const Sidebar: React.FC<SidebarProps> = ({ conversations, currentConversation, setCurrentConversation }) => {
+const Sidebar: React.FC<SidebarProps> = ({
+  conversations,
+  currentConversation,
+  setCurrentConversation,
+}) => {
   return (
     <div className=" bg-neutral-100 h-screen w-96 relative border border-r-neutral-300">
       <div className="flex absolute top-0 left-0 right-0 h-14 justify-center">
@@ -23,17 +29,19 @@ const Sidebar: React.FC<SidebarProps> = ({ conversations, currentConversation, s
       </div>
       <div className="absolute top-14 left-0 right-0 bottom-0 p-2">
         <div className="grid gap-2">
-          {conversations.map((conversation) => (
-            <Converasation
-              img={"default-pfp.jpg"}
-              username={conversation.users[0].username}
-              lastMessage="Hello World"
-              dateLastMessage={new Date()}
-              isSelected={conversation === currentConversation}
-              onClick={() => setCurrentConversation(conversation)}
-              key={conversation.id}
-            />
-          ))}
+          {conversations.map((conversation) => {
+            return (
+              <Converasation
+                img={"default-pfp.jpg"}
+                username={conversation.users[0].username}
+                lastMessage={conversation.lastMessageSent.message}
+                dateLastMessage={new Date(conversation.lastMessageSent.created_at)}
+                isSelected={conversation === currentConversation}
+                onClick={() => setCurrentConversation(conversation)}
+                key={conversation.id}
+              />
+            );
+          })}
         </div>
       </div>
     </div>

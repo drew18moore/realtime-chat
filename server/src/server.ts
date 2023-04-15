9688046db55f2db639ab4f43507e0afa9603e550
Drew <dcm752052@gmail.com>
2023-04-15 10:32:39 -0400

    Removed test routes from server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,7 @@
-import express, { Request, Response } from "express";
+import express, { Request } from "express";
 import { Server } from "socket.io";
 import http from "http";
 import cors from "cors";
-import { db } from "./db";
 import authRouter from "./routes/auth";
 import usersRouter from "./routes/users";
 import { corsOptions } from "./config/corsOptions";
@@ -20,47 +19,6 @@ const io = new Server(server, {
 
 app.use(cors<Request>(corsOptions));
 app.use(express.json());
-
-app.post("/friend", async (req: Request, res: Response) => {
-  const { userId1, userId2 } = req.body;
-  try {
-    const userUpdatedUser1 = await db.user.update({
-      where: { id: userId1 },
-      data: {
-        friends: {
-          connect: { id: userId2 },
-        }
-      }
-    })
-    const userUpdatedUser2 = await db.user.update({
-      where: { id: userId2 },
-      data: {
-        friends: {
-          connect: { id: userId1 },
-        },
-        friendOf: {
-          connect: { id: userId1 },
-        }
-      }
-    })
-    res.status(200).json({ message: "Friend added" });
-  } catch (err) {
-    res.status(500).json(err)
-  }
-});
-
-app.get("/friends", async (req, res) => {
-  const { userId } = req.body;
-  try {
-    const user = await db.user.findUnique({
-      where: { id: Number(userId) },
-      include: { friends: true }
-    })
-    res.status(200).json({ user });
-  } catch (err) {
-    res.status(500).json(err)
-  }
-})
 
 app.use("/api/auth", authRouter)
 app.use("/api/users", usersRouter)

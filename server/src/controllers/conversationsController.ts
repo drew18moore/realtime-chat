2a26d1c45fe40fa0e0c3ed0c88070296b1f120e3
Drew <dcm752052@gmail.com>
2023-04-18 21:21:24 -0400

    Modified new conversation endpoint to return existing conversation if one already exists.

--- server/src/controllers/conversationsController.ts
+++ server/src/controllers/conversationsController.ts
@@ -2,31 +2,87 @@ import { Request, Response } from "express";
 import { db } from "../db";
 
 export const newConversation = async (req: Request, res: Response) => {
-  const { recipientId, message, authorId } = req.body;
+  const { recipientId, authorId } = req.body;
   const recipientIdParsed = parseInt(recipientId);
   const authorIdParsed = parseInt(authorId);
   try {
+    // Check if a conversation exists
+    const existingConversation = await db.conversation.findMany({
+      where: {
+        OR: [
+          {
+            AND: [
+              { creatorId: authorIdParsed },
+              { joinerId: recipientIdParsed },
+            ],
+          },
+          {
+            AND: [
+              { creatorId: recipientIdParsed },
+              { joinerId: authorIdParsed },
+            ],
+          },
+        ],
+      },
+      select: {
+        id: true,
+        title: true,
+        creator: {
+          select: {
+            id: true,
+            username: true,
+          }
+        },
+        joiner: {
+          select: {
+            id: true,
+            username: true,
+          }
+        },
+      }
+    });
+    if (existingConversation.length > 0) {
+      const response = {
+        ...existingConversation[0],
+        recipient: existingConversation[0].creator,
+        creator: undefined,
+        joiner: undefined,
+        messages: undefined,
+      }
+      return res.status(200).json(response)
+    }
     const conversation = await db.conversation.create({
       data: {
         creator: { connect: { id: authorIdParsed } },
         joiner: { connect: { id: recipientIdParsed } },
-        // messages: {
-        //   create: {
-        //     message,
-        //     authorId: authorIdParsed,
-        //     receiverId: recipientIdParsed,
-        //   },
-        // },
       },
-      include: {
-        creator: true,
-        joiner: true,
-        messages: true,
-      },
+      select: {
+        id: true,
+        title: true,
+        creator: {
+          select: {
+            id: true,
+            username: true,
+          }
+        },
+        joiner: {
+          select: {
+            id: true,
+            username: true,
+          }
+        },
+      }
     });
-    res.status(200).json(conversation);
+    const response = {
+      ...conversation,
+      recipient: conversation.joiner,
+      creator: undefined,
+      joiner: undefined,
+      messages: undefined,
+    }
+    res.status(201).json(response);
   } catch (err) {
-    console.error(err)
+    console.error(err);
     res.status(500).json({ message: err });
   }
 };

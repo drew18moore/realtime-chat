227de2bec2423ad79bea7171a89995b7f36240f9
Drew <dcm752052@gmail.com>
2023-04-28 14:50:40 -0400

    Moved useMutation code from Contact.tsx into new useNewConversation hook.

--- client/src/components/Contact.tsx
+++ client/src/components/Contact.tsx
@@ -1,49 +1,22 @@
 import { FC } from "react";
-import useAxiosPrivate from "../hooks/useAxiosPrivate";
-import { useAuth } from "../contexts/AuthContext";
-import { useNavigate } from "react-router-dom";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNewConversation } from "../hooks/useConversations";
 
 interface ContactProps {
   img: string;
   id: number;
   username: string;
-  clearSearch: () => void
+  clearSearch: () => void;
 }
 
 const Contact: FC<ContactProps> = ({ img, id, username, clearSearch }) => {
-  const axiosPrivate = useAxiosPrivate();
-  const queryClient = useQueryClient()
-  const { currentUser } = useAuth();
-  const navigate = useNavigate();
-
-  const { mutate: newConversation } = useMutation(() => {
-    return axiosPrivate.post("/api/conversations/new", {
-      creatorId: currentUser?.id,
-      joinerId: id,
-    })
-  }, {
-    onSuccess: (data) => {
-      clearSearch()
-      const prevConversations: any = queryClient.getQueryData(["conversations"])
-      console.log(prevConversations?.data);
-      if (!prevConversations.data.some((conv: Conversation) => conv.id === data.data.id)) {
-        queryClient.setQueryData(["conversations"], {
-          ...prevConversations,
-          data: [...prevConversations.data, data.data]
-        })
-      }
-      const state = { recipient: data.data.recipient };
-      navigate(`/${data.data.id}`, { state })
-    },
-    onError: (err) => {
-      console.log("ERROR", err);
-    }
-  });
+  const { mutate: newConversation } = useNewConversation(id);
 
   return (
     <div
-      onClick={() => newConversation()}
+      onClick={() => {
+        newConversation();
+        clearSearch();
+      }}
       className="rounded-xl flex gap-3 p-2 items-center cursor-pointer hover:bg-neutral-300"
     >
       <div className="w-10 h-10 rounded-full overflow-hidden flex-shrink-0">
--- client/src/hooks/useConversations.ts
+++ client/src/hooks/useConversations.ts
@@ -1,7 +1,8 @@
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import useAxiosPrivate from "./useAxiosPrivate"
 import { useAuth } from "../contexts/AuthContext";
 import { AxiosResponse } from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const useGetConversations = () => {
   const axiosPrivate = useAxiosPrivate();
@@ -13,4 +14,34 @@ export const useGetConversations = () => {
       console.error(err)
     },
   })
+}
+
+export const useNewConversation = (joinerId: number) => {
+  const axiosPrivate = useAxiosPrivate();
+  const { currentUser } = useAuth();
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  return useMutation(() => {
+    return axiosPrivate.post("/api/conversations/new", {
+      creatorId: currentUser?.id,
+      joinerId,
+    })
+  }, {
+    onSuccess: (data) => {
+      const prevConversations: any = queryClient.getQueryData(["conversations"])
+      console.log(prevConversations?.data);
+      if (!prevConversations.data.some((conv: Conversation) => conv.id === data.data.id)) {
+        queryClient.setQueryData(["conversations"], {
+          ...prevConversations,
+          data: [...prevConversations.data, data.data]
+        })
+      }
+      const state = { recipient: data.data.recipient };
+      navigate(`/${data.data.id}`, { state })
+    },
+    onError: (err) => {
+      console.log("ERROR", err);
+    }
+  });
 }

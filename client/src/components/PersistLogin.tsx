8b7e610a0e324edee0dea4a6659f725bc7c35133
Drew <dcm752052@gmail.com>
2023-05-04 22:49:35 -0400

    Styled the PersistLogin loading state and added RotatingLines component.

--- client/src/components/PersistLogin.tsx
+++ client/src/components/PersistLogin.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useAuth } from "../contexts/AuthContext";
 import api from "../api/api";
+import { RotatingLines } from "react-loader-spinner";
 
 const PersistLogin = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -26,8 +27,11 @@ const PersistLogin = () => {
   return (
     <>
       {isLoading ? (
-        <div className="">
-          <p>Please wait for the server to load.</p>
+        <div className="h-[calc(100svh)] grid place-items-center">
+          <div className="border border-neutral-300 rounded-xl p-4 w-fit h-fit flex flex-col gap-2 items-center">
+            <p className="text-black text-lg">Please wait for the server to load.</p>
+            <RotatingLines strokeColor="gray" width="24" />
+          </div>
         </div>
       ) : (
         <Outlet />

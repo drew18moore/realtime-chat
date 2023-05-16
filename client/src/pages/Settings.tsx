ded0dc8dacfbfce7b6d601be10d394392d552b9c
Drew <dcm752052@gmail.com>
2023-05-16 19:03:35 -0400

    Gave settings main content div a max-w. Removed placeholder content.

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -4,16 +4,6 @@ import { useNavigate } from "react-router-dom";
 const Settings = () => {
   const navigate = useNavigate();
 
-  const content = [];
-
-  for (let i = 0; i < 20; i++) {
-    content.push(
-      <div key={i} className="bg-red-100 p-2">
-        <h2>Heading {i + 1}</h2>
-      </div>
-    );
-  }
-
   return (
     <div className="h-[calc(100svh)]">
       <div className="flex items-center gap-3 h-14 px-5 bg-transparent border border-b-neutral-200 border-x-0 border-t-0">
@@ -26,7 +16,9 @@ const Settings = () => {
         <h1 className="text-2xl">Settings</h1>
       </div>
       <div className="overflow-y-auto" style={{ height: "calc(100svh - 3.5rem)" }}>
-        <div className="grid gap-2 p-2">{content}</div>
+        <div className="grid gap-2 p-2 max-w-2xl mx-auto">
+          
+        </div>
       </div>
     </div>
   );

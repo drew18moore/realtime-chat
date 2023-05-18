49fbe7abfca28c5922e72174b544eb0773e1a800
Drew <dcm752052@gmail.com>
2023-05-17 22:08:35 -0400

    Added dark mode styles to settings page.

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -5,15 +5,15 @@ const Settings = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="h-[calc(100svh)]">
-      <div className="flex items-center gap-3 h-14 px-5 bg-transparent border border-b-neutral-200 border-x-0 border-t-0">
+    <div className="h-[calc(100svh)] dark:bg-black">
+      <div className="flex items-center gap-3 h-14 px-5 bg-transparent border border-b-neutral-200 border-x-0 border-t-0 dark:border-b-neutral-800">
         <button
-          className="hover:bg-neutral-200 h-11 aspect-square flex items-center justify-center rounded-full p-2.5"
+          className="hover:bg-neutral-200 h-11 aspect-square flex items-center justify-center rounded-full p-2.5 dark:text-white dark:hover:bg-neutral-800"
           onClick={() => navigate(-1)}
         >
           <BiArrowBack size={"100%"} />
         </button>
-        <h1 className="text-2xl">Settings</h1>
+        <h1 className="text-2xl dark:text-white">Settings</h1>
       </div>
       <div className="overflow-y-auto" style={{ height: "calc(100svh - 3.5rem)" }}>
         <div className="grid gap-2 p-2 max-w-2xl mx-auto">

fed95ca359c483b8e93e277e43989e6264a431cf
Drew <dcm752052@gmail.com>
2023-05-18 22:01:09 -0400

    Created layout for theme switcher in settings.

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -15,9 +15,43 @@ const Settings = () => {
         </button>
         <h1 className="text-2xl dark:text-white">Settings</h1>
       </div>
-      <div className="overflow-y-auto" style={{ height: "calc(100svh - 3.5rem)" }}>
+      <div
+        className="overflow-y-auto"
+        style={{ height: "calc(100svh - 3.5rem)" }}
+      >
         <div className="grid gap-2 p-2 max-w-2xl mx-auto">
-          
+          <div className="flex flex-col gap-4 mt-4">
+            <h2 className="text-blue-600">General</h2>
+            <h3 className="">Choose theme</h3>
+            <div className="flex justify-around">
+              <div className="w-40 flex flex-col gap-2">
+                <div className="border border-neutral-200 rounded-xl overflow-hidden cursor-pointer">
+                  <img src="theme-lightmode.svg" alt="" className="w-full" />
+                </div>
+                <div className="flex items-center gap-2">
+                  <input
+                    type="radio"
+                    className="accent-blue-600 w-5 h-5 cursor-pointer"
+                    id="lightmode"
+                  />
+                  <label htmlFor="lightmode" className="cursor-pointer">Light</label>
+                </div>
+              </div>
+              <div className="w-40 flex flex-col gap-2">
+                <div className="border border-neutral-200 rounded-xl overflow-hidden cursor-pointer">
+                  <img src="theme-darkmode.svg" alt="" className="w-full" />
+                </div>
+                <div className="flex items-center gap-2">
+                  <input
+                    type="radio"
+                    className="accent-blue-600 w-5 h-5 cursor-pointer"
+                    id="darkmode"
+                  />
+                  <label htmlFor="darkmode" className="cursor-pointer">Dark</label>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>

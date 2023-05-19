04b863f9f277d74b53cc965a96d1b4748d49ec4a
Drew <dcm752052@gmail.com>
2023-05-18 22:27:28 -0400

    Fully implemented theme switcher functionality.

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -27,28 +27,32 @@ const Settings = () => {
             <h3 className="dark:text-white">Choose theme</h3>
             <div className="flex justify-around dark:text-white">
               <div className="w-40 flex flex-col gap-2">
-                <div className="border border-neutral-200 dark:border-neutral-800 rounded-xl overflow-hidden cursor-pointer">
+                <div onClick={(e) => {setTheme("light")}} className="border border-neutral-200 dark:border-neutral-800 rounded-xl overflow-hidden cursor-pointer">
                   <img src="theme-lightmode.svg" alt="" className="w-full" />
                 </div>
                 <div className="flex items-center gap-2">
                   <input
                     type="radio"
                     className="accent-blue-600 w-5 h-5 cursor-pointer"
+                    onChange={(e) => setTheme(e.target.value)}
                     id="lightmode"
+                    value="light"
                     checked={theme === "light"}
                   />
                   <label htmlFor="lightmode" className="cursor-pointer">Light</label>
                 </div>
               </div>
               <div className="w-40 flex flex-col gap-2">
-                <div className="border border-neutral-200 dark:border-neutral-800 rounded-xl overflow-hidden cursor-pointer">
+                <div onClick={(e) => {setTheme("dark")}} className="border border-neutral-200 dark:border-neutral-800 rounded-xl overflow-hidden cursor-pointer">
                   <img src="theme-darkmode.svg" alt="" className="w-full" />
                 </div>
                 <div className="flex items-center gap-2">
                   <input
                     type="radio"
                     className="accent-blue-600 w-5 h-5 cursor-pointer"
+                    onChange={(e) => setTheme(e.target.value)}
                     id="darkmode"
+                    value="dark"
                     checked={theme === "dark"}
                   />
                   <label htmlFor="darkmode" className="cursor-pointer">Dark</label>

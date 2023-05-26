2203f2c4922e75cbffb357271c06af53fe0274f2
Drew <dcm752052@gmail.com>
2023-05-26 18:16:00 -0400

    Created endpoint for deleting messages by id.

--- server/src/controllers/messagesController.ts
+++ server/src/controllers/messagesController.ts
@@ -89,3 +89,24 @@ export const getMessagesInConversation = async (
     res.status(500).json({ message: err });
   }
 };
+
+export const deleteMessage = async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id)
+  try {
+    const message = await db.message.findUnique({
+      where: { id }
+    });
+
+    if (!message) {
+      return res.status(404).json({ message: "Message not found" });
+    }
+
+    await db.message.delete({
+      where: { id }
+    });
+
+    res.status(200).json({ message: "Message deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err })
+  }
+}
--- server/src/routes/messages.ts
+++ server/src/routes/messages.ts
@@ -1,10 +1,12 @@
 import express, { Router } from "express";
-import { getMessagesInConversation, newMessage } from "../controllers/messagesController";
+import { deleteMessage, getMessagesInConversation, newMessage } from "../controllers/messagesController";
 import { verifyJWT } from "../middleware/verifyJWT";
 
 const messagesRouter: Router = express.Router();
 
 messagesRouter.post("/new", verifyJWT, newMessage);
 messagesRouter.get("/", verifyJWT, getMessagesInConversation)
+messagesRouter.route("/:id")
+  .delete(verifyJWT, deleteMessage)
 
 export default messagesRouter;

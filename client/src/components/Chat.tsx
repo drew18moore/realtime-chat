2162af3e65869e1afadfd248043b44d766608856
Drew <dcm752052@gmail.com>
2024-06-21 15:02:01 -0400

    User can now select an image for their message and have a preview of the image displayed above the text input.

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -20,6 +20,7 @@ const Chat = () => {
   const messagesContainerRef = useRef<HTMLDivElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
   const [messageToEdit, setMessageToEdit] = useState<Message | null>(null);
+  const [imgBase64, setImgBase64] = useState("");
 
   const LIMIT = 20;
   const { data: messages, fetchNextPage } = useGetMessagesInfinite(
@@ -79,9 +80,9 @@ const Chat = () => {
   }, [messageHasBeenSent, messageHasBeenUpdated]);
 
   return (
-    <div className="relative h-[calc(100svh)]">
+    <div className="relative h-[calc(100svh)] flex flex-col">
       {/* Header bar */}
-      <div className="flex items-center gap-3 absolute top-0 right-0 left-0 h-14 px-5 sm:px-10 border border-b-neutral-200 border-x-0 border-t-0 dark:border-b-neutral-800">
+      <div className="flex items-center gap-3 h-14 px-5 py-3 sm:px-10 border border-b-neutral-200 border-x-0 border-t-0 dark:border-b-neutral-800">
         <button
           className="hover:bg-neutral-200 h-11 aspect-square flex items-center justify-center rounded-full p-2.5 sm:hidden dark:text-white dark:hover:bg-neutral-800"
           onClick={() => navigate(-1)}
@@ -102,7 +103,7 @@ const Chat = () => {
         </h1>
       </div>
 
-      <div className="absolute top-14 bottom-20 w-full flex flex-col justify-end">
+      <div className="flex-grow-1 min-h-0 w-full flex flex-col justify-end">
         <div
           ref={messagesContainerRef}
           className="grid gap-2 p-2 pb-8 overflow-y-auto relative"
@@ -144,6 +145,8 @@ const Chat = () => {
           messageToEdit={messageToEdit}
           setMessageToEdit={setMessageToEdit}
           inputRef={inputRef}
+          imgBase64={imgBase64}
+          setImgBase64={setImgBase64}
         />
       )}
     </div>
